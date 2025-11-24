5842559324770032aa550a47713b837be18b0dd9
Lenovo\The Thinh <thinhnt42@fpt.com>
2025-11-24 23:13:15 +0700

    Revert "feat(server): add threshold manual"
    This reverts commit b81fef4a833debd01c4efb6ae23e3d3a686ccc6b.

--- src/main/java/com/example/demo/repository/ThresholdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.model.entity.ThresholdSetting;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-@Repository
-public interface ThresholdRepository extends BaseRepository<ThresholdSetting, Long>{
-    Optional<ThresholdSetting> findByDeviceUid(String deviceUid);
-}
